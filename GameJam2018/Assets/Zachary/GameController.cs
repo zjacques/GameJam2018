879d27b1a57748fbb485bcb7d91f03049aa445be
zjacques <zachary.jacques160@gmail.com>
2018-01-27 11:30:40 +0000

    added scene transition, and second scene for later levels

--- GameJam2018/Assets/Zachary/GameController.cs
+++ GameJam2018/Assets/Zachary/GameController.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-
+    
     public GameObject shout;
+    public Image black;
+    public Animator anim;
+    bool transition = false;
+
     public int score = 0;
     public int level = 1;
+
+    public int peopleSaved = 0;
+
     public bool hasClicked = false;
     GameObject[] NPCs;
 
@@ -36,13 +45,31 @@ public class GameController : MonoBehaviour {
 
         if(hasClicked)
         {
-            bool finished = true;
+            //bool finished = true;
             foreach (GameObject npc in NPCs)
             {
                 //Check all the objects to see when everything has stopped moving
                 /*if (npc.GetComponent("Mover").moving)
                     finished = false;*/
+                
+            }
+            if (transition == false)
+            {
+                StartCoroutine(Fading());
+                transition = true;
             }
         }
+    }
+
+    IEnumerator Fading()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadSceneAsync("Scene2");
+        anim.SetBool("Fade", false);
+        hasClicked = false;
+        transition = false;
+        yield return null;
     }
 }
