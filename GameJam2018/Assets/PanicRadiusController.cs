49bc4c9adc1d4f43748f4888c1ce74f3ca7addf9
C00204864 <c00204864@itcarlow.ie>
2018-01-27 17:36:19 +0000

    Radius of influnce now created and visible

---
+++ GameJam2018/Assets/PanicRadiusController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanicRadiusController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CircleCollider2D c = gameObject.GetComponent<CircleCollider2D> ();
+		transform.localScale = new Vector2(c.radius, c.radius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
