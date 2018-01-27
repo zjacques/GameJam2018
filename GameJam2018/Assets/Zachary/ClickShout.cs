40565f3b1cb905d0a1ac8bf434c75ab67052b6de
zjacques <zachary.jacques160@gmail.com>
2018-01-27 09:24:45 +0000

    added expanding circle on mouse click

---
+++ GameJam2018/Assets/Zachary/ClickShout.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickShout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("GrowShrink");
+        Destroy(gameObject, 2f);
+	}
+	
+    IEnumerator GrowShrink()
+    {
+        Vector3 startingScale = transform.localScale;
+        Vector3 expandedScale = new Vector3(1, 1, 1);
+        float time = 1;
+        float timeSoFar = 0;
+
+        while (timeSoFar < time)
+        {
+            transform.localScale = Vector3.Lerp(startingScale, expandedScale, (timeSoFar / time));
+            timeSoFar += Time.deltaTime;
+            yield return null;
+        }
+        timeSoFar = 0;
+        while (timeSoFar < time)
+        {
+            transform.localScale = Vector3.Lerp(expandedScale, startingScale, (timeSoFar / time));
+            timeSoFar += Time.deltaTime;
+            yield return null;
+        }
+
+    }
+
+}
---
+++ GameJam2018/Assets/Zachary/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject shout;
+    public int score = 0;
+    public int level = 1;
+    public bool hasClicked = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && hasClicked == false)
+        {
+            Debug.Log("Left Click");
+
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+            Debug.Log(pz);
+            Instantiate(shout, pz, Quaternion.identity);
+            hasClicked = true;
+        }
+    }
+}
