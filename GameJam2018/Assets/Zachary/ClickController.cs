b083e775997bd37744dd78cdc2f396e7e81f0f9c
zjacques <zachary.jacques160@gmail.com>
2018-01-27 08:57:18 +0000

    Added Zachary scene and images

---
+++ GameJam2018/Assets/Zachary/ClickController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickController : MonoBehaviour {
+
+    public GameObject shout;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Click");
+
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+            Debug.Log(pz);
+            Instantiate(shout, pz, Quaternion.identity);
+        }
+	}
+}
