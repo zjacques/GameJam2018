341d9e539a15e40d662be8a7be757c63b49a89c0
C00204864 <c00204864@itcarlow.ie>
2018-01-27 15:24:08 +0000

    Added NPC Panic mode

--- GameJam2018/Assets/Liam/Mover.cs
+++ GameJam2018/Assets/Liam/Mover.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour {
 
+	public float speed;
+	public float panicSpeed;
+
 	private Vector2 velocity;
 	private Rigidbody2D rb2d;
 	private System.Random randNum = new System.Random();
 
+	private float angle;
+
 	// Use this for initialization
 	void Start () {
+
+		angle = Random.Range (0, Mathf.PI * 2);
+
+		velocity = new Vector2 ();
+
+		velocity.x = Mathf.Sin (angle) * speed;
+		velocity.y = Mathf.Cos (angle) * speed;
+
 		rb2d = gameObject.GetComponent<Rigidbody2D>();
-		velocity = new Vector2(Random.Range(-50, 50), Random.Range(-50, 50));
+		//velocity = new Vector2(Random.Range(-speed, speed), Random.Range(-speed, speed));
 		rb2d.AddForce(velocity);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.CompareTag("NPC"))
+		{
+			//Vector2 newVelocity = new Vector2 (-gameObject.GetComponent<Rigidbody2D> ().velocity.x, -gameObject.GetComponent<Rigidbody2D> ().velocity.y);
+			//gameObject.GetComponent<Rigidbody2D> ().velocity = newVelocity;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		Debug.Log ("trig");
+		GameObject other = otherCollider.gameObject;
+		Vector2 otherPos = otherCollider.gameObject.transform.position;
+		Vector2 vecBetween = new Vector2 (transform.position.x - otherPos.x, transform.position.y - otherPos.y); //transform.position - otherPos;
+		vecBetween.Normalize();
+		if (other.gameObject.CompareTag ("Panic")) 
+		{
+			float angle = Mathf.PI / 2.0f - Mathf.Atan (vecBetween.y / vecBetween.x);
+			vecBetween.x *= panicSpeed;
+			vecBetween.y *= panicSpeed;
+			rb2d.velocity = new Vector2 ();
+			rb2d.AddForce(vecBetween);
+		}
 	}
 }
