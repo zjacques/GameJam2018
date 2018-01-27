c272fec1304d47e04b08aa579f34c78ee05be549
LiamHickey <c00204864@itcarlow.ie>
2018-01-27 11:34:33 +0000

    Added Bouncing Scripts and a basic mover script

---
+++ GameJam2018/Assets/Liam/BoundaryDetector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDetector : MonoBehaviour {
+
+	private Collider2D m_collider;
+	private Vector2 spriteSize;
+
+	// Use this for initialization
+	void Start () {
+		m_collider = GetComponent<Collider2D> ();
+		spriteSize = GetComponent<SpriteRenderer>().bounds.size;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		GameObject other = otherCollider.gameObject;
+		double xMax = spriteSize.x / 2.0; // Half as bounds.size.x is the entire width of the collider
+		double xMin = -spriteSize.x / 2.0;
+		double yMax = spriteSize.y / 2.0;
+		double yMin = -spriteSize.y / 2.0;
+		Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+		double otherWidth = otherCollider.bounds.size.x / 2.0;
+		double otherHeight = otherCollider.bounds.size.y / 2.0;
+
+		if(other.GetComponent<Transform>().position.x + otherWidth> xMax || other.GetComponent<Transform>().position.x - otherWidth < xMin)
+		{
+			rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+		}
+		if(other.GetComponent<Transform>().position.y - otherHeight < yMin || other.GetComponent<Transform>().position.y + otherHeight > yMax)
+		{
+			rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+		}
+	}
+}
---
+++ GameJam2018/Assets/Liam/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	private Vector2 velocity;
+	private Rigidbody2D rb2d;
+	private System.Random randNum = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+		velocity = new Vector2(Random.Range(-50, 50), Random.Range(-50, 50));
+		rb2d.AddForce(velocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
